1c879f65c8b7292b3f1446b8ccf21eed137f3d86
jouzune <dmatson@student.neumont.edu>
2017-08-25 10:04:35 -0600

    RESTTransport

--- src/com/_17od/upm/transport/RESTTransport.java
+++ src/com/_17od/upm/transport/RESTTransport.java
@@ -3,7 +3,7 @@
  * Copyright (C) 2005-2013 Adrian Smith
  *
  * This file is part of Universal Password Manager.
- *   
+ *
  * Universal Password Manager is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or
@@ -20,12 +20,11 @@
  */
 package com._17od.upm.transport;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
+import java.io.*;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.net.URLEncoder;
+import java.nio.charset.Charset;
 
 import org.apache.commons.codec.binary.Base64;
 import org.apache.commons.httpclient.Credentials;
@@ -34,6 +33,7 @@ import org.apache.commons.httpclient.HttpException;
 import org.apache.commons.httpclient.HttpStatus;
 import org.apache.commons.httpclient.UsernamePasswordCredentials;
 import org.apache.commons.httpclient.auth.AuthScope;
+import org.apache.commons.httpclient.methods.ByteArrayRequestEntity;
 import org.apache.commons.httpclient.methods.GetMethod;
 import org.apache.commons.httpclient.methods.PostMethod;
 import org.apache.commons.httpclient.methods.multipart.FilePart;
@@ -45,12 +45,12 @@ import org.apache.commons.httpclient.protocol.ProtocolSocketFactory;
 import com._17od.upm.util.Preferences;
 
 
-public class HTTPTransport extends Transport {
+public class RESTTransport extends Transport {
 
     private HttpClient client;
 
 
-    public HTTPTransport() {
+    public RESTTransport() {
 
         client = new HttpClient();
 
@@ -74,14 +74,14 @@ public class HTTPTransport extends Transport {
             String proxyUserName = Preferences.get(Preferences.ApplicationOptions.HTTP_PROXY_USERNAME);
             String proxyPassword = Preferences.get(Preferences.ApplicationOptions.HTTP_PROXY_PASSWORD);
             String decodedPassword = new String(Base64.decodeBase64(proxyPassword.getBytes()));
-            
+
             if (isNotEmpty(proxyHost)) {
                 int proxyPort = 0;
                 if (isNotEmpty(proxyPortStr)) {
                     proxyPort = Integer.parseInt(proxyPortStr);
                     client.getHostConfiguration().setProxy(proxyHost, proxyPort);
                     if (isNotEmpty(proxyUserName) && isNotEmpty(proxyPassword)) {
-                        client.getState().setProxyCredentials(AuthScope.ANY, 
+                        client.getState().setProxyCredentials(AuthScope.ANY,
                                 new UsernamePasswordCredentials(proxyUserName, decodedPassword));
                     }
                 }
@@ -94,51 +94,44 @@ public class HTTPTransport extends Transport {
     public void put(String targetLocation, File file) throws TransportException {
         put(targetLocation, file, null, null);
     }
-    
-    
+
+
     public void put(String targetLocation, File file, String username, String password) throws TransportException {
 
-        targetLocation = addTrailingSlash(targetLocation) + "upload.php";
-        
+        targetLocation = addTrailingSlash(targetLocation) + "database";
+
         PostMethod post = new PostMethod(targetLocation);
 
         //This part is wrapped in a try/finally so that we can ensure
-        //the connection to the HTTP server is always closed cleanly 
+        //the connection to the HTTP server is always closed cleanly
         try {
-            
-            Part[] parts = {
-                    new FilePart("userfile", file)
-            };
-            post.setRequestEntity(
-                    new MultipartRequestEntity(parts, post.getParams())
-            );
-
-            //Set the HTTP authentication details
-            if (username != null) {
-                Credentials creds = new UsernamePasswordCredentials(new String(username), new String(password));
-                URL url = new URL(targetLocation);
-                AuthScope authScope = new AuthScope(url.getHost(), url.getPort());
-                client.getState().setCredentials(authScope, creds);
-                client.getParams().setAuthenticationPreemptive(true);
+            if (username == null) {
+                throw new TransportException("No username");
+            }
+            if (password == null) {
+                throw new TransportException("No password");
             }
 
-            // This line makes the HTTP call
+            //Set the HTTP Basic authentication details
+            post.addRequestHeader("Authorization", getBasicAuth(username, password));
+
+            FileInputStream fis = new FileInputStream(file);
+            byte[] data = new byte[(int) file.length()];
+            fis.read(data);
+            fis.close();
+
+            post.addParameter("database", URLEncoder.encode(new String(data), "UTF-8"));
+
             int status = client.executeMethod(post);
-            
-            // I've noticed on Windows (at least) that PHP seems to fail when moving files on the first attempt
-            // The second attempt works so lets just do that
-            if (status == HttpStatus.SC_OK && post.getResponseBodyAsString().equals("FILE_WASNT_MOVED")) {
-                status = client.executeMethod(post);                
+
+            switch (status) {
+                case HttpStatus.SC_CREATED:
+                    break;
+                case HttpStatus.SC_BAD_REQUEST:
+                    throw new TransportException(post.getResponseBodyAsString());
+                default: break;
             }
 
-            if (status != HttpStatus.SC_OK) {
-                throw new TransportException("There's been some kind of problem uploading a file to the HTTP server.\n\nThe HTTP error message is [" + HttpStatus.getStatusText(status) + "]");
-            }
-            
-            if (!post.getResponseBodyAsString().equals("OK") ) {
-                throw new TransportException("There's been some kind of problem uploading a file to the HTTP server.\n\nThe error message is [" + post.getResponseBodyAsString() + "]");
-            }
-            
         } catch (FileNotFoundException e) {
             throw new TransportException(e);
         } catch (MalformedURLException e) {
@@ -150,44 +143,45 @@ public class HTTPTransport extends Transport {
         } finally {
             post.releaseConnection();
         }
-        
+
     }
 
 
     public byte[] get(String url, String fileName) throws TransportException {
         return get(url, fileName, null, null);
     }
-    
-    
+
+
     public byte[] get(String url, String fileName, String username, String password) throws TransportException {
         url = addTrailingSlash(url);
         return get(url + fileName, username, password);
     }
-    
-    
+
+
     public byte[] get(String url, String username, String password) throws TransportException {
 
         byte[] retVal = null;
 
         GetMethod method = new GetMethod(url);
-        
-        //This part is wrapped in a try/finally so that we can ensure
-        //the connection to the HTTP server is always closed cleanly 
-        try {
 
-            //Set the authentication details
-            if (username != null) {
-                Credentials creds = new UsernamePasswordCredentials(new String(username), new String(password));
-                URL urlObj = new URL(url);
-                AuthScope authScope = new AuthScope(urlObj.getHost(), urlObj.getPort());
-                client.getState().setCredentials(authScope, creds);
-                client.getParams().setAuthenticationPreemptive(true);
+        //This part is wrapped in a try/finally so that we can ensure
+        //the connection to the HTTP server is always closed cleanly
+        try {
+            if (username == null) {
+                throw new TransportException("No username");
+            }
+            if (password == null) {
+                throw new TransportException("No password");
             }
 
-            int statusCode = client.executeMethod(method);
+            //Set the HTTP Basic authentication details
+            method.setRequestHeader("Authorization", getBasicAuth(username, password));
+            int status = client.executeMethod(method);
 
-            if (statusCode != HttpStatus.SC_OK) {
-                throw new TransportException("There's been some kind of problem getting the URL [" + url + "].\n\nThe HTTP error message is [" + HttpStatus.getStatusText(statusCode) + "]");
+            switch (status) {
+                case HttpStatus.SC_OK:
+                    break;
+                default: throw new TransportException(method.getResponseBodyAsString());
             }
 
             retVal = method.getResponseBody();
@@ -201,22 +195,22 @@ public class HTTPTransport extends Transport {
         } finally {
             method.releaseConnection();
         }
-        
+
         return retVal;
 
     }
 
-    
+
     public File getRemoteFile(String remoteLocation, String fileName) throws TransportException {
         return getRemoteFile(remoteLocation, fileName, null, null);
     }
 
-    
+
     public File getRemoteFile(String remoteLocation) throws TransportException {
         return getRemoteFile(remoteLocation, null, null);
     }
 
-    
+
     public File getRemoteFile(String remoteLocation, String fileName, String httpUsername, String httpPassword) throws TransportException {
         remoteLocation = addTrailingSlash(remoteLocation);
         return getRemoteFile(remoteLocation + fileName, httpUsername, httpPassword);
@@ -236,7 +230,7 @@ public class HTTPTransport extends Transport {
         }
     }
 
-    
+
     public void delete(String targetLocation, String name, String username, String password) throws TransportException {
 
         targetLocation = addTrailingSlash(targetLocation) + "deletefile.php";
@@ -245,7 +239,7 @@ public class HTTPTransport extends Transport {
         post.addParameter("fileToDelete", name);
 
         //This part is wrapped in a try/finally so that we can ensure
-        //the connection to the HTTP server is always closed cleanly 
+        //the connection to the HTTP server is always closed cleanly
         try {
 
             //Set the authentication details
@@ -261,7 +255,7 @@ public class HTTPTransport extends Transport {
             if (status != HttpStatus.SC_OK) {
                 throw new TransportException("There's been some kind of problem deleting a file on the HTTP server.\n\nThe HTTP error message is [" + HttpStatus.getStatusText(status) + "]");
             }
-            
+
             if (!post.getResponseBodyAsString().equals("OK") ) {
                 throw new TransportException("There's been some kind of problem deleting a file to the HTTP server.\n\nThe error message is [" + post.getResponseBodyAsString() + "]");
             }
@@ -283,7 +277,7 @@ public class HTTPTransport extends Transport {
         delete(targetLocation, name, null, null);
     }
 
-    
+
     private String addTrailingSlash(String url) {
         if (url.charAt(url.length() - 1) != '/') {
             url = url + '/';
@@ -291,13 +285,19 @@ public class HTTPTransport extends Transport {
         return url;
     }
 
-    
+
     private boolean isNotEmpty(String stringToCheck) {
         boolean retVal = false;
         if (stringToCheck != null && !stringToCheck.trim().equals("")) {
             retVal = true;
         }
         return retVal;
+    }
+
+    private String getBasicAuth(String username, String password) {
+        String auth = username + ":" + password;
+        byte[] basicEncoded = Base64.encodeBase64(auth.getBytes(Charset.forName("ISO-8859-1")));
+        return String.format("Basic %s", new String(basicEncoded));
     }
 
 }
